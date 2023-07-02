a38da8bdc7530d8d3aea8eeaeb24586a1081eddb
Ilya Koloskov <ilblesskol@gmail.com>
2023-07-02 23:10:53 +0300

    refactor style

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,12 @@ import Post from './Post/Post'
 import styles from './MyPosts.module.sass'
 
 function MyPosts() {
-  console.log(`${styles.posts} qwe`)
   return (
     <div className={`${styles.posts} box`}>
-      <h2 className={styles.posts__title}>My posts</h2>
+      <h2 className={`${styles.posts__title} h2`}>My posts</h2>
       <div className={styles.posts__inner}>
         <textarea className={styles.posts__textarea} placeholder="Add posts:"></textarea>
-        <button className={`${styles.posts__btn} btn`}>Add post</button>
+        <button className={`${styles.posts__btn} btn display-36`}>Add post</button>
       </div>
       <div className={styles.posts__list}>
         <Post />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,22 +5,22 @@ function Profile() {
   return (
     <section className={styles.profile}>
       <div className={`${styles.profile__wrap} box`}>
-        <h1 className={styles.profile__title}>Ilya Koloskov</h1>
+        <h1 className={`${styles.profile__title} h1`}>Ilya Koloskov</h1>
         <div className={styles.profile__dob}>09 / 03 / 1998</div>
         <img className={styles.profile__avatar} src="/images/profile.jpg" />
         <div className={styles.profile__info}>
           <ul className="profile__info-list">
             <li className="profile__info-item">
-              <strong className={styles.strong}>Nationality: </strong>
+              <strong className={styles.profile__strong}>Nationality: </strong>
               <span className="profile__info-span">Russia</span>
             </li>
             <li className="profile__info-item">
-              <strong className={styles.strong}>Passion: </strong>
+              <strong className={styles.profile__strong}>Passion: </strong>
               <span className="profile__info-span">Frontend, music, sport</span>
             </li>
           </ul>
         </div>
-        <div className={`${styles.profile__btn} btn`}>Send message</div>
+        <div className={`${styles.profile__btn} btn display-46`}>Send message</div>
       </div>
       <MyPosts />
     </section>
