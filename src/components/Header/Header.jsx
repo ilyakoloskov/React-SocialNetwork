7fd90b53426e9ddf019c3d2ba6e5844668a92cd8
Ilya Koloskov <ilblesskol@gmail.com>
2023-07-11 22:45:22 +0300

    add layout player

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,31 @@ function Header() {
         <span className="menu__breadcrumb">menu</span>
         <span className={`${styles.header__burger} burger`}>X</span>
       </div> */}
-      <div className={`${styles.header__player} box`}>player</div>
+      <div className={`${styles.header__player} box player`}>
+        <div className="player__controls">
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/prev.svg" alt="" />
+          </div>
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/play.svg" alt="" />
+          </div>
+          {/* <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/pause.svg" alt="" />
+          </div> */}
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/next.svg" alt="" />
+          </div>
+        </div>
+        <img className="player__cover" src="/images/profile.jpg" alt="" />
+        <div className="player__currents">
+          <span className="player__start">00:00</span>
+          <div className="player__duration"></div>
+          <span className="player__end">02:38</span>
+        </div>
+        <div className="player__volume">
+          <input type="range" name="" id="" />
+        </div>
+      </div>
     </header>
   )
 }
--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,9 +2,31 @@ import styles from './Player.module.sass'
 
 function Player() {
   return (
-    <div className={styles.player}>
-      <div className={`${styles.player__container} container`}>
-        <div className={styles.player__wrapper}>Player</div>
+    <div className="player-scroll">
+      <div className="player-scroll__wrapper container">
+        <div className="player__controls">
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/prev.svg" alt="" />
+          </div>
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/play.svg" alt="" />
+          </div>
+          {/* <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/pause.svg" alt="" />
+          </div> */}
+          <div className="player__btn">
+            <img className="player__btn-icon" src="/icons/next.svg" alt="" />
+          </div>
+        </div>
+        <img className="player__cover" src="/images/profile.jpg" alt="" />
+        <div className="player__currents">
+          <span className="player__start">00:00</span>
+          <div className="player__duration"></div>
+          <span className="player__end">02:38</span>
+        </div>
+        <div className="player__volume">
+          <input type="range" name="" id="" />
+        </div>
       </div>
     </div>
   )
